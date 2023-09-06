36dc3ac16e32a65966825bebbbf965d1762caa5d
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-09-06 12:06:49 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/create_group_chat/create_group_chat_model.dart
+++ lib/pages/create_group_chat/create_group_chat_model.dart
@@ -4,7 +4,7 @@ import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
-import '/pages/chat_page/chat_page_widget.dart';
+import '/pages/chat/chat_page/chat_page_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
